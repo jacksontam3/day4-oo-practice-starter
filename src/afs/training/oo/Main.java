1530ad799964d0a576d1f0d5b10ad01913dc97b9
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 11:23:02 +0800

    [oo] feat: AC2

--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Bus bus = new Bus("Cool Bus", 25);
         bus.SpeedUp();
+        Truck truck =  new Truck("Big Truck", 20);
+        truck.SpeedUp();
     }
 }
---
+++ src/afs/training/oo/Truck.java
@@ -0,0 +1,18 @@
+package afs.training.oo;
+
+public class Truck {
+
+    private String name;
+
+    private int speed;
+
+    public Truck(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public void SpeedUp() {
+        speed += 2;
+        System.out.printf("%s: speed up to %d km/h\n",name, speed);
+    }
+}
